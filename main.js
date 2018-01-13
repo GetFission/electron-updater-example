2bc258f4e3ad7765ac235a5d618cf5b76d37fb8f
simplyahmazing <ahmad19526@gmail.com>
2018-01-13 17:10:16 -0500

    remove unneeded code

--- main.js
+++ main.js
@@ -139,7 +139,6 @@ const feedURL = `http://localhost:8000/api/v1/updates/?${urlInfo}`
 console.log('calc url', getRequestInfo())
 autoUpdater.setFeedURL(feedURL)
 console.log(`Updater Feed url is, ${autoUpdater.getFeedURL()}`)
-  // autoUpdater.checkForUpdatesAndNotify(); // TODO: lookup impl
   autoUpdater.checkForUpdates();
 });
 
@@ -147,31 +146,3 @@ ipcMain.on('check-for-updates', function () {
   console.log('Triggering update check..')
   autoUpdater.checkForUpdates();
 })
-
-//-------------------------------------------------------------------
-// Auto updates - Option 2 - More control
-//
-// For details about these events, see the Wiki:
-// https://github.com/electron-userland/electron-builder/wiki/Auto-Update#events
-//
-// The app doesn't need to listen to any events except `update-downloaded`
-//
-// Uncomment any of the below events to listen for them.  Also,
-// look in the previous section to see them being used.
-//-------------------------------------------------------------------
-// app.on('ready', function()  {
-//   autoUpdater.checkForUpdates();
-// });
-// autoUpdater.on('checking-for-update', () => {
-// })
-// autoUpdater.on('update-available', (info) => {
-// })
-// autoUpdater.on('update-not-available', (info) => {
-// })
-// autoUpdater.on('error', (err) => {
-// })
-// autoUpdater.on('download-progress', (progressObj) => {
-// })
-// autoUpdater.on('update-downloaded', (info) => {
-//   autoUpdater.quitAndInstall();  
-// })
